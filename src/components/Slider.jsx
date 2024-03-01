e157b49d3a388bc195db29b9705b51f265de94c7
haquedot <haquedot@gmail.com>
2024-03-01 18:34:15 +0530

    added button to slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,5 +1,5 @@
 // Import Swiper React components
-import React, { useRef, useState } from 'react';
+// import React, { useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { product } from '../utils/data';
 import { getImageUrl } from '../utils/utils';
@@ -11,9 +11,10 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 import "../styles/style-Slider.css"
+import "../App.css"
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, Navigation } from 'swiper/modules';
 
 const Slider = () => {
   return (
@@ -22,30 +23,30 @@ const Slider = () => {
         <Swiper
           // slidesPerView={6}
           // spaceBetween={10}
-          pagination={{
-            clickable: true,
-          }}
+          // pagination={{
+          //   clickable: true,
+          // }}
           navigation={true}
           autoplay={{
-            delay: 2500,
+            delay: 4000,
             disableOnInteraction: false,
           }}
           breakpoints={{
-            
+
             640: {
               slidesPerView: 1,
               spaceBetween: 10,
             },
             768: {
-              slidesPerView: 4,
+              slidesPerView: 3,
               spaceBetween: 10,
             },
             1024: {
-              slidesPerView: 5,
+              slidesPerView: 6,
               spaceBetween: 10,
             },
           }}
-          modules={[Autoplay, Pagination, Navigation]}
+          modules={[Autoplay, Navigation]}
           className="mySwiper mb-3"
         >
           {product.map((product, index) => (
@@ -57,9 +58,12 @@ const Slider = () => {
                     <span className='w-50 text-end'>wishlist</span>
                   </div>
                   <img src={getImageUrl(product)} alt={product.imageId} srcSet="" />
-                  <div className="d-block text-start">
-                    <h4>{product.name}</h4>
+                  <div className="d-block text-start mb-3">
+                    <h3>{product.price}$</h3>
+                    <h5>{product.name}</h5>
                   </div>
+                  <button  className='btn_white w-100'>Add to cart</button>
+
                 </div>
 
               </Card>
--- src/utils/data.js
+++ src/utils/data.js
@@ -2,81 +2,97 @@ export const product = [
     {
         id: 0,
         imageId: "cauliflower_1",
+        price: 100,
         name: "Pea",
     },
     {
         id: 1,
         imageId: "cauliflower_1",
+        price: 100,
         name: "Carrots",
     },
     {
         id: 2,
         imageId: "cauliflower_1",
+        price: 100,
         name: "Bananas",
     },
     {
         id: 3,
         imageId: "cauliflower_1",
+        price: 100,
         name: "Zucchini",
     },
     {
         id: 4,
         imageId: "cauliflower_1",
+        price: 100,
         name: "Eggplant",
     },
     {
         id: 5,
         imageId: "cauliflower_1",
+        price: 100,
         name: "Oranges",
     },
     {
         id: 6,
         imageId: "cauliflower_1",
+        price: 100,
         name: "Grapes",
     },
     {
         id: 7,
         imageId: "cauliflower_1",
+        price: 100,
         name: "Tomatoes",
     },
     {
         id: 8,
         imageId: "cauliflower_1",
+        price: 100,
         name: "Tomatoes",
     },
     {
         id: 9,
         imageId: "cauliflower_1",
+        price: 100,
         name: "Tomatoes",
     },
     {
         id: 10,
         imageId: "cauliflower_1",
+        price: 100,
         name: "Tomatoes",
     },
     {
         id: 11,
         imageId: "cauliflower_1",
+        price: 100,
         name: "Tomatoes",
     },
     {
         id: 12,
         imageId: "cauliflower_1",
+        price: 100,
         name: "Tomatoes",
     },
     {
         id: 13,
         imageId: "cauliflower_1",
+        price: 100,
         name: "Tomatoes",
     },
     {
         id: 14,
         imageId: "cauliflower_1",
+        price: 100,
         name: "Tomatoes",
     },
     {
         id: 15,
         imageId: "cauliflower_1",
+        price: 100,
         name: "Tomatoes",
     }
 
