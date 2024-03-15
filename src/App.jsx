ef9acea7faa5b6913ced19219d535579d1a0d33d
Meraj <haquedot@gmail.com>
2024-03-15 14:36:57 +0530

    contact

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import CustomNavbar from './components/CustomNavbar'
 import Home from './views/Home'
 import Footer from './components/Footer'
 import Login from './views/Login'
-import Contact from './views/Contact'
+// import Contact from './views/Contact'
 import Register from './views/Register'
 import ProductDetails from './views/ProductDetails'
 import Cart from './views/Cart'
 import Admin from './views/adminDashboard/Admin'
+import Contact from './views/Contact'
 
 function App() {
 
@@ -33,38 +34,3 @@ function App() {
 }
 
 export default App
-
-
-
-
-
-// import React, { useState, useEffect } from 'react';
-// import { useLocation, useParams } from 'react-router-dom';
-// import { product } from '../utils/data';
-
-// export default function Cart() {
-//     const { productId } = useParams();
-//     const [productItem, setProductItem] = useState(null);
-
-//     useEffect(() => {
-//         const foundProduct = product.find(item => item.id === parseInt(productId));
-//         if (foundProduct) {
-//             setProductItem(foundProduct);
-//         } else {
-//             console.log('Product not found');
-//         }
-//     }, [productId]);
-
-//     return (
-//         <div>
-//             <h1>Shopping Cart</h1>
-//             {productItem && (
-//                 <>
-//                     <h2>Product ID: {productId}</h2>
-//                     <p>Product Name: {productItem.name}</p>
-//                     <p>Price: ₹{productItem.price}</p>
-//                 </>
-//             )}
-//         </div>
-//     );
-// }
--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,9 +1,46 @@
-const Contact=()=>{
+import React from 'react';
+import { Row, Col, Form } from 'react-bootstrap';
+
+export default function Contact() {
+
     return (
         <>
-            <h1>Contact Details page</h1>
+
+            <Row>
+                <Col md={7}>
+                    <div className="rounded p-3 bg-body-tertiary">
+                        <h2>Contact Us</h2>
+                        <Form>
+                            <Row>
+                                <Col md={6}>
+                                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                        <Form.Label>Full Name</Form.Label>
+                                        <Form.Control type="text" placeholder="Name" />
+                                    </Form.Group>
+                                </Col>
+                                <Col md={6}>
+                                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                        <Form.Label>Email Address</Form.Label>
+                                        <Form.Control type="email" placeholder="Email" />
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                <Form.Label>Subject</Form.Label>
+                                <Form.Control type="text" placeholder="Subject" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Message</Form.Label>
+                                <Form.Control as="textarea" placeholder="Message" rows={3} />
+                            </Form.Group>
+                        </Form>
+                    </div>
+                </Col>
+                <Col md={5}>
+                </Col>
+            </Row>
+
         </>
-    )
+    );
 }
 
-export default Contact;
