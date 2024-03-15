99f70c47ee997c4134afe703b0c142454b707715
Meraj <haquedot@gmail.com>
2024-03-15 16:43:03 +0530

    created admin dashboard

---
+++ src/views/adminDashboard/AcountDetails.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+
+
+export default function AccountDetails() {
+
+  return (
+    <>
+      <h1>Admin AccountDetails</h1>
+
+    </>
+  );
+}
---
+++ src/views/adminDashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import "../../App.css"
+
+
+export default function Dashboard() {
+
+  return (
+    <>
+      <h1>Admin Dashboard</h1>
+
+    </>
+  );
+}
