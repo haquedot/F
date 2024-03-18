1bad14c0dc43a9e54e1a5d17209506a2e79e47f0
Meraj <haquedot@gmail.com>
2024-03-18 15:35:55 +0530

    added content in dashboard

--- src/views/adminDashboard/Dashboard.jsx
+++ src/views/adminDashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Card } from 'react-bootstrap';
 import "../../App.css"
 
 
@@ -7,7 +7,31 @@ export default function Dashboard() {
 
   return (
     <>
-      <h1>Admin Dashboard</h1>
+      <Card className='p-3'>
+        <h3>Dashboard</h3>
+        <Row>
+          <Col md={6}>
+            <Card className='p-2'>
+              <h5 className='m-0'><i className="bi bi-cart-fill me-2"></i>Orders</h5>
+              <hr />
+              <p className="d-flex">
+                Total <span className='ms-auto'>0</span>
+              </p>
+              <p className="d-flex text-success">
+                Completed <span className='ms-auto'>0</span>
+              </p>
+              <p className="d-flex text-secondary">
+                Pending <span className='ms-auto'>0</span>
+              </p>
+              <p className="d-flex text-primary">
+                Processing <span className='ms-auto'>0</span>
+              </p>
+              <p className="d-flex text-danger">
+                Cancelled <span className='ms-auto'>0</span>
+              </p>
+            </Card></Col>
+        </Row>
+      </Card>
 
     </>
   );
