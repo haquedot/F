f8aba1437a6c507e30dcc5fc664cfacda4cf83ca
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-04-02 02:29:52 +0530

    adding the user and the admin account details

--- src/views/adminDashboard/AcountDetails.jsx
+++ src/views/adminDashboard/AcountDetails.jsx
@@ -1,53 +1,101 @@
-import React, { useState } from 'react';
-import { Form, Row, Col, Card } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-
-
+import Cookies from "js-cookie";
+import { decode } from "jwt-js-decode";
+import { useEffect, useState } from "react";
+import { Form, Row, Col, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function AccountDetails() {
+  const [userDetails, setUserDetails] = useState({
+    email: "",
+    username: "",
+    firstName: "",
+    lastName: "",
+  });
+  useEffect(() => {
+    handleAccountDetails();
+  }, []);
+  const handleAccountDetails = () => {
+    try {
+      const token = Cookies.get("token");
+      const decoded = decode(token);
+      const username = decoded.payload.user.username.split(" ");
+      setUserDetails({
+        email: decoded.payload.user.email,
+        username: decoded.payload.user.username,
+        firstName: username[0],
+        lastName: username[1],
+      });
+      console.log(decoded.payload.user);
+    } catch (error) {
+      console.log("error:", error);
+    }
+  };
 
   return (
     <>
-      <Card className='p-3'>
+      <Card className="p-3">
         <h4>Edit Account Details</h4>
         <Form>
-
           <Row>
             <Col md={6}>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
                 <Form.Label>First name</Form.Label>
-                <Form.Control type="text" placeholder="First name" />
+                <Form.Control
+                  type="text"
+                  placeholder="First name"
+                  id="first-name"
+                  name="firstname"
+                  value={userDetails.firstName}
+                />
               </Form.Group>
             </Col>
             <Col md={6}>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput2"
+              >
                 <Form.Label>Last name</Form.Label>
-                <Form.Control type="text" placeholder="Last name" />
+                <Form.Control
+                  type="text"
+                  placeholder="Last name"
+                  id="last-name"
+                  name="lastname"
+                  value={userDetails.lastName}
+                />
               </Form.Group>
             </Col>
           </Row>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Email address" />
+            <Form.Control
+              type="email"
+              placeholder="Email address"
+              id="email"
+              name="email"
+              value={userDetails.email}
+            />
           </Form.Group>
 
           <h5>Password Change</h5>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput4">
             <Form.Label>Current password</Form.Label>
-            <Form.Control type='password' />
+            <Form.Control type="password" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput5">
             <Form.Label>New password</Form.Label>
-            <Form.Control type='password' />
+            <Form.Control type="password" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput6">
             <Form.Label>Confirm new password</Form.Label>
-            <Form.Control type='password' />
+            <Form.Control type="password" />
           </Form.Group>
 
           <div className="text-end">
             <Link>
-              <button className='btn_filled'>Save changes</button>
+              <button className="btn_filled">Save changes</button>
             </Link>
           </div>
         </Form>
--- src/views/userDashboard/AcountDetails.jsx
+++ src/views/userDashboard/AcountDetails.jsx
@@ -1,53 +1,95 @@
-import React, { useState } from 'react';
-import { Form, Row, Col, Card } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-
-
+import React, { useState ,useEffect} from "react";
+import { Form, Row, Col, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Cookies from "js-cookie";
+import { decode } from "jwt-js-decode";
 
 export default function AccountDetails() {
-
+  const [userDetails, setUserDetails] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+    username: "",
+  });
+  const handleUserDetails = () => {
+    const token = Cookies.get("token");
+    const jwtDecode = decode(token);
+    const userName = jwtDecode.payload.user.username.split(" ");
+    setUserDetails({
+      email: jwtDecode.payload.user.email,
+      username: jwtDecode.payload.user.username,
+      firstName: jwtDecode.payload.user.firstname,
+      lastName:jwtDecode.payload.user.lastname,
+    });
+  };
+  useEffect(() => {
+    handleUserDetails();
+  }, []);
   return (
     <>
-      <Card className='p-3'>
+      <Card className="p-3">
         <h4>Edit Account Details</h4>
         <Form>
-
           <Row>
             <Col md={6}>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
                 <Form.Label>First name</Form.Label>
-                <Form.Control type="text" placeholder="First name" />
+                <Form.Control
+                  type="text"
+                  placeholder="First name"
+                  id="first-name"
+                  name="firstname"
+                  value={userDetails.firstName}
+                />
               </Form.Group>
             </Col>
             <Col md={6}>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput2"
+              >
                 <Form.Label>Last name</Form.Label>
-                <Form.Control type="text" placeholder="Last name" />
+                <Form.Control
+                  type="text"
+                  placeholder="Last name"
+                  id="last-name"
+                  name="lastname"
+                  value={userDetails.lastName}
+                />
               </Form.Group>
             </Col>
           </Row>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Email address" />
+            <Form.Control
+              type="email"
+              placeholder="Email address"
+              id="email"
+              name="email"
+              value={userDetails.email}
+            />
           </Form.Group>
 
           <h5>Password Change</h5>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput4">
             <Form.Label>Current password</Form.Label>
-            <Form.Control type='password' />
+            <Form.Control type="password" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput5">
             <Form.Label>New password</Form.Label>
-            <Form.Control type='password' />
+            <Form.Control type="password" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput6">
             <Form.Label>Confirm new password</Form.Label>
-            <Form.Control type='password' />
+            <Form.Control type="password" />
           </Form.Group>
 
           <div className="text-end">
             <Link>
-              <button className='btn_filled'>Save changes</button>
+              <button className="btn_filled">Save changes</button>
             </Link>
           </div>
         </Form>
