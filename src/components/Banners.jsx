6e5969055b381816572275e99fb2eed7cbab3d95
Meraj <haquedot@gmail.com>
2024-04-03 00:23:30 +0530

    created banner-1

---
+++ src/components/Banners.jsx
@@ -0,0 +1,9 @@
+function Banners(props) {
+    return (
+        <>
+            <img src={props.bannerSrc} alt="banner_1" className="rounded-3 w-100 my-3" />
+        </>
+    )
+}
+
+export default Banners;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,13 +1,15 @@
+import Banners from "../components/Banners";
 import HeroSection from "../components/HeroSection";
 import Slider from "../components/Slider";
 import TopSeller from "../components/TopSeller";
-
+import banner_1 from '../assets/images/banner_1.svg'
 
 function Home() {
     return (
         <>
             <HeroSection />
             <Slider />
+            <Banners bannerSrc={banner_1}/>
             <TopSeller/>
         </>
     )
