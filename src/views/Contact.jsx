a3a3d16d829a3ff51ad268e692068b623660da11
Meraj <haquedot@gmail.com>
2024-04-03 22:31:57 +0530

    added map

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -7,7 +7,7 @@ export default function Contact() {
         <>
 
             <Row>
-                <Col md={7}>
+                <Col md={6}>
                     <div className="rounded p-3 bg-body-tertiary">
                         <h2>Contact Us</h2>
                         <Form>
@@ -36,7 +36,8 @@ export default function Contact() {
                         </Form>
                     </div>
                 </Col>
-                <Col md={5}>
+                <Col md={6}>
+                <iframe className='w-100' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3806.6706877904135!2d78.35762354033605!3d17.427584517893113!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb93c915a6b2ab%3A0x422046359cc10ec6!2sBoys%20hostel%203!5e0!3m2!1sen!2sin!4v1712163416486!5m2!1sen!2sin" width="600" height="450" style={{border:0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </Col>
             </Row>
 
